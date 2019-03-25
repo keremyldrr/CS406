a1b6a631a26e4aae3144a0d74c288d51ef964a7d
kerem <keremyildirir@sabanciuniv.edu>
2019-03-26 00:13:40 +0300

    deneyler

--- deneyci.py
+++ deneyci.py
@@ -5,12 +5,12 @@ import os
 data, temp = os.pipe()
 st = "Num Threads,Result,Time,"
 for i in range(50):
-    s = subprocess.check_output("./out3 input4.txt", stdin = data, shell = True)
+    s = subprocess.check_output("./out0 input2.txt", stdin = data, shell = True)
     print("Iteration ",i)
     st +=(s.decode("utf-8")) 
 
 
-f1 = open("O3_results_input4.csv","w")
+f1 = open("O0_results_input2.csv","w")
 
 for line in st:
     f1.write(line)
