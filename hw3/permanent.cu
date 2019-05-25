8d874a68f2383681bccf8f098cab49363992450b
kerem <keremyildirir@sabanciuniv.edu>
2019-05-25 22:40:38 +0300

    perma

---
+++ hw3/permanent.cu
@@ -0,0 +1,229 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <omp.h>
+#include "cuda_error_handle.h"
+using namespace std;
+typedef long long int ll;
+
+__global__ void permanent(int *M,float *X,ll N,float *d_p,ll  chunkSize,int noChunks,ll lim,int numT)
+{
+  extern __shared__ float shared_x [];
+
+  ll gl_tid = threadIdx.x + blockIdx.x * blockDim.x;
+  ll block_id = threadIdx.x;
+
+  int cnt = 0;
+  for(int i=block_id;i<N*numT;i+=numT){
+    shared_x[i] = X[cnt];
+    cnt++;
+  }
+  int *Mstart = (int *)&shared_x[N*numT];
+  if(block_id == 0){
+    for(int i=0;i<N;i++)
+    {
+      for(int j=0;j<N;j++)
+	{
+	  Mstart[i + j*N] = M[i + j*N];
+	  
+	}
+      
+    }
+  }
+  __syncthreads();
+
+  
+
+  
+  ll  r = (gl_tid)*chunkSize; // each chunk in X array is of size N;
+    
+    ll  y = r ^ (r>>1LL);
+    ll  numSetBits= __popcll(y);
+    
+    for( int f=0;f<numSetBits;f++)
+      {
+      ll bit = __ffsll(y);
+      
+      for (int  q = 0; q < N; q++)
+	{
+	  shared_x[block_id + q*numT] += Mstart[q +N*(bit-1)];
+
+	}
+      y = y^(1LL << (bit-1));
+      
+      } ///value initialization
+    ////////////////////////////////////////////
+
+     
+
+    ll start = gl_tid*chunkSize+1;
+    ll end = start+chunkSize;
+    ll prodSign = start & 1LL;
+    ll limiter = lim;//(1LL << (N-1));
+    double res = 0;
+  if(prodSign)
+    prodSign=-1LL;
+  else
+    prodSign = 1LL;
+  for( ll ind=start;ind<end && ind < limiter;ind++)
+    {
+      
+      ll y = (ind ^ (ind >> 1LL));
+      ll j = ind-1;
+      ll z = __ffsll(y ^ (j ^ (j >> 1LL)));
+      ll mask = (1LL << (z-1));
+      ll s = y & mask;
+      
+      if(!s)
+	s=-1;
+      else
+	s=1;
+      float prodX = 1;
+      for( ll i=0;i<N;i++)
+	{
+	  shared_x[i*numT + block_id] += s*Mstart[i + N*(z-1)];
+	  prodX *= shared_x[i*numT + block_id ];
+	}
+      res +=  prodSign * prodX;
+      prodSign*=-1;
+    }
+  atomicAdd(d_p, res);
+}                  
+
+void usage()
+{
+  cout << "USAGE: ./exec <filename>" << endl;
+  exit(0);
+}
+int main(int argc, const char** argv)
+{
+  
+  if(argc != 2)
+    usage();
+
+  string line;
+
+  const char* filename = argv[1];
+  ifstream input (filename);
+  if(input.fail())
+    return 0;
+
+
+  int N;
+  int **M;
+  getline(input,line);
+  N = atoi(line.c_str());
+
+  M = new int*[N];
+  for(int i = 0; i < N; i ++)
+    M[i] = new int[N];
+
+
+  int linectr = 0;
+  while(getline(input,line)){
+    stringstream ss(line);
+    int temp;
+    int ctr = 0;
+    while(ss >> temp)
+      M[linectr][ctr++] = temp;
+
+    linectr++;
+  }
+  
+  float start, end;
+  cudaSetDevice(0);  
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties(&prop, 0);
+  ll threads = prop.maxThreadsPerBlock;
+  ll max_blocks = prop.maxGridSize[0];
+  ll mem = prop.sharedMemPerBlock; //bytes
+  ll float_size = sizeof(float);
+  int numThreads  = 256;//(mem - N*N*sizeof(int))/(N*sizeof(float)); //THREADS PER BLOCK
+  threads =  numThreads;
+  int numBlocks = 1024*64;//max_blocks/N;
+  //  threads=1;
+  ll totalNumThreads = numThreads * numBlocks;
+  
+  //  ll shift =1; 
+  ll lim = (1LL<< (N-1));
+  ll chunkSize = lim/(totalNumThreads);
+  
+  if(chunkSize == 0)
+    chunkSize = 1;
+  ll noChunks = totalNumThreads;//lim/chunkSize ;
+   cout << "Chunksize is : " << chunkSize <<  " lim is " <<lim << " and there are "<<noChunks << " chunks and "<<totalNumThreads << " threads "<<  endl;  
+  float *local_x_values = new float [ N];
+  int *new_m = new int[N * N];
+  float *column_sum = new float[N];
+  float *d_local;
+  int *d_m;
+  float *d_p;
+  float *errortest = new float [totalNumThreads];//change it to num threads
+  ll errorSize = sizeof(float )*(totalNumThreads);
+  float *d_errortest;
+  CudaSafeCall(cudaMalloc((void **)&d_local,sizeof(float)*( N)));
+  CudaSafeCall(cudaMalloc((void **)&d_m,sizeof(int)*(N * N)));
+  CudaSafeCall(cudaMalloc((void **)&d_p,sizeof(float)));
+  CudaSafeCall(cudaMalloc((void **)&d_errortest,errorSize));
+  for(int i = 0;i<N;i++)
+    {
+      for(int j = 0;j<N;j++)
+	{
+	  new_m[i + j*N] = M[i][j];
+	} //M initialization for GPU
+    }
+
+  
+ 
+
+  float p = 1;
+  
+  for(int i=0;i<N;i++)
+    {
+      column_sum[i] = 0;
+      
+      for(int j=0;j<N;j++)
+	{
+	  column_sum[i]+=M[i][j];
+	}
+      
+      p *= (M[i][N-1] - column_sum[i]/2);
+      
+    }
+  // cout << "INITIAL P " << p << endl;
+  
+  for(int j=0;j<N;j++) // X vector init
+	{
+	  local_x_values[j] = (M[j][N-1] - float(column_sum[j]/2));
+	}
+  
+
+
+      CudaSafeCall(cudaMemcpy(d_local,local_x_values,sizeof(float)*( N),cudaMemcpyHostToDevice) );
+      CudaSafeCall(cudaMemcpy(d_m,new_m,sizeof(int)*N*N,cudaMemcpyHostToDevice));
+      CudaSafeCall(cudaMemcpy(d_p,&p,sizeof(float),cudaMemcpyHostToDevice));
+  
+ ////PREPROCESSING COMPLETE
+  start = omp_get_wtime();      
+
+  int shared_size =  N * threads * sizeof(float) + N*N*sizeof(int);
+  
+  permanent<<<numBlocks,threads, shared_size >>>(d_m,d_local,N,d_p,chunkSize,noChunks,lim,numThreads);
+  CudaCheckError();
+  end = omp_get_wtime();
+  
+  CudaSafeCall(cudaMemcpy(&p,d_p,sizeof(float),cudaMemcpyDeviceToHost));
+
+  
+
+  cout << "FINAL P "<<p << endl;
+  float result = (4 * (N % 2) - 2) * p;
+  cout << "GPU \tResult:" << result << "\tTime:" << end - start << " s" << endl;
+    //cout << t << "," << result << "," << end - start <<",\n";
+    
+    
+  
+  return 0;
+    
+}
